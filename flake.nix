c75bcd45c8b55d7a983540d5609c035adc8fc7c8
Alex Martens <alex@thinglab.org>
2022-04-09 09:38:00 -0700

    flake: improve

--- flake.nix
+++ flake.nix
@@ -6,43 +6,20 @@
   outputs = { self, nixpkgs }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      nativeBuildInputs = with pkgs; [ pkg-config ];
-      buildInputs = with pkgs; [ libusb1 udev ];
       cargoToml = nixpkgs.lib.importTOML ./Cargo.toml;
     in
     {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        buildInputs = buildInputs ++ nativeBuildInputs;
-      };
-
-      packages.x86_64-linux.${cargoToml.package.name} = pkgs.rustPlatform.buildRustPackage {
-        pname = cargoToml.package.name;
-        inherit (cargoToml.package) version;
-
-        src = ./.;
-
-        RUSTFLAGS = "-D warnings";
-
-        inherit buildInputs nativeBuildInputs;
-
-        cargoLock.lockFile = ./Cargo.lock;
-
-        doCheck = true;
-
-        meta = with pkgs.lib; {
-          inherit (cargoToml.package) description;
-          homepage = cargoToml.package.repository;
-          license = with licenses; [ mit ];
-        };
-      };
+      packages.x86_64-linux.${cargoToml.package.name} = pkgs.callPackage ./package.nix { };
 
       packages.x86_64-linux.default = self.packages.x86_64-linux.${cargoToml.package.name};
+
+      devShells.x86_64-linux.default = self.packages.x86_64-linux.default;
 
       checks.x86_64-linux = {
         format = pkgs.runCommand "format"
           {
-            inherit nativeBuildInputs;
-            buildInputs = with pkgs; [ rustfmt cargo ] ++ buildInputs;
+            inherit (self.packages.x86_64-linux.default) nativeBuildInputs;
+            buildInputs = with pkgs; [ rustfmt cargo ] ++ self.packages.x86_64-linux.default.buildInputs;
           } ''
           ${pkgs.rustfmt}/bin/cargo-fmt fmt --manifest-path ${./.}/Cargo.toml -- --check
           ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check ${./.}

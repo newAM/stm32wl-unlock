6550a53228069162e51fe6862205a9600f420660
Alex Martens <alex@thinglab.org>
2022-11-24 18:16:04 -0800

    crane: follow flake-utils

--- flake.nix
+++ flake.nix
@@ -3,11 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+
     flake-utils.url = "github:numtide/flake-utils";
+
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+    crane.inputs.flake-utils.follows = "flake-utils";
   };
 
   outputs = {
